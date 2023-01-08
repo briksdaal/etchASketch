9ea05b5591f8f39a996d1fd3954e33d46ba253b9
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-01-08 04:13:11 +0200

    Add option for hover/no hover and vanishing ink

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
 const DEFAULTNUMBEROFSQUARES = 16;
+const TRANSITIONINTIME = 0.7;
+const TRANSITIONOUTTIME = 3;
+const MAXSQUARES = 100;
+
+let boolOnHover = false;
+let boolVanishing = false;
 
 const squaresContainer = document.querySelector(".squares-container");
 const input = document.querySelector("input");
 
-initiateBoard();
+drawBoard(DEFAULTNUMBEROFSQUARES);
 
-function initiateBoard() {
-  createSquares(DEFAULTNUMBEROFSQUARES);
-  input.value = DEFAULTNUMBEROFSQUARES;
-}
-
-function createSquares(num) {
-  squaresContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-  const square = document.createElement("div");
-  // square.classList.add("square");
-
-  // square.addEventListener("mouseenter", () => console.log("a"));
-  // square.addEventListener("mouseleave", removeHover);
-
-  const arr = [];
-  for (let i = 0; i < num * num; i++) {
-    tempSquare = square.cloneNode(true);
-    tempSquare.addEventListener("mouseenter", (e) => onEnterSquare(e.target));
-    tempSquare.addEventListener("mouseleave", (e) => onLeaveSquare(e.target));
-    arr.push(tempSquare);
+function drawBoard(newNumOfSquares) {
+  clearSquares();
+  if (newNumOfSquares >= MAXSQUARES) {
+    newNumOfSquares = MAXSQUARES;
+  } else if (newNumOfSquares < 1) {
+    newNumOfSquares = 1;
   }
-  squaresContainer.append(...arr);
-}
-
-function onEnterSquare(hoveredSquare) {
-  hoveredSquare.style.backgroundColor = "#000";
-}
-
-function onLeaveSquare(hoveredSquare) {
-  
+  input.value = newNumOfSquares;
+  createSquares(newNumOfSquares);
 }
 
 function clearSquares() {
@@ -42,11 +28,64 @@ function clearSquares() {
   }
 }
 
+function createSquares(num) {
+  squaresContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  const square = document.createElement("div");
+
+  const arr = [];
+  for (let i = 0; i < num * num; i++) {
+    tempSquare = square.cloneNode(true);
+    tempSquare.addEventListener("mouseenter", (e) => onEnterSquare(e));
+    if (boolVanishing) {
+      tempSquare.addEventListener("mouseleave", (e) => onLeaveSquare(e));
+    }
+    arr.push(tempSquare);
+  }
+  squaresContainer.append(...arr);
+}
+
+function onEnterSquare(hoveredSquareEvent) {
+  if (boolOnHover || hoveredSquareEvent.buttons > 0) {
+    const hoveredSquare = hoveredSquareEvent.target;
+    hoveredSquare.style.backgroundColor = "rgb(0, 0, 0)";
+    hoveredSquare.style.transitionDuration = TRANSITIONINTIME + "s";
+  }
+}
+
+function onLeaveSquare(hoveredSquareEvent) {
+  const hoveredSquare = hoveredSquareEvent.target;
+  setTimeout(() => {
+    hoveredSquare.style.transitionDuration = TRANSITIONOUTTIME + "s";
+    hoveredSquare.style.backgroundColor = "rgb(255, 255, 255)";
+  }, TRANSITIONINTIME * 1000);
+}
+
+// input field redraw
 input.addEventListener("input", (e) => {
-  clearSquares();
-  let newNumOfSquares = e.target.value;
-  if (newNumOfSquares >= 100) newNumOfSquares = 100;
-  else if (newNumOfSquares < 1) newNumOfSquares = 1;
-  input.value = newNumOfSquares;
-  createSquares(newNumOfSquares);
+  drawBoard(e.target.value);
 });
+
+// reset button redraw
+resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", () => {
+  const newNumOfSquares = prompt("Enter new sq", 16);
+  drawBoard(newNumOfSquares);
+});
+
+btnVanishing = document.querySelector(".btn-vanishing");
+btnOnHover = document.querySelector(".btn-on-hover");
+
+btnVanishing.addEventListener("click", (e) => {
+  e.target.classList.toggle("btn-pushed");
+  boolVanishing = !boolVanishing;
+  drawBoard(input.value);
+});
+
+btnOnHover.addEventListener("click", (e) => {
+  e.target.classList.toggle("btn-pushed");
+  boolOnHover = !boolOnHover;
+  drawBoard(input.value);
+});
+
+window.addEventListener("mousedown", () => (isMouseDown = true));
+window.addEventListener("mouseup", () => (isMouseDown = false));
